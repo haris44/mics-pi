758cff54c035910ed81ba52afa66989aebd3234a
Alexandre B <alexandre@hublot.io>
2019-02-06 17:01:19 +0100

    add timer

--- captor.py
+++ captor.py
@@ -1,22 +1,23 @@
 # coding=utf-8
 
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
 Trig = 17
-nbPrise = 1000
+nbPrise = 100
 
 
 GPIO.setup(Trig, GPIO.IN)
 
 
-
-
 def take():
     total = 0
     nb = 0
+
     while nb < nbPrise:
+        time.sleep(0.5)
         total = total + GPIO.input(Trig)
         nb = nb + 1
     return total
