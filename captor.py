e24bbc366f5194e527f1386815b94c1c1579e554
BERTRAND Alexandre <lxndr.bertrand@gmail.com>
2019-02-08 09:02:17 +0000

    fix date en send to api

--- captor.py
+++ captor.py
@@ -2,6 +2,12 @@
 
 import RPi.GPIO as GPIO
 import time
+import datetime
+import requests
+import json
+import pytz
+
+local_tz = pytz.timezone('Europe/Paris')
 
 GPIO.setmode(GPIO.BCM)
 
@@ -11,21 +17,31 @@ nbPrise = 100
 
 GPIO.setup(Trig, GPIO.IN)
 
+def utc_to_local(utc_dt):
+    local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+    return local_tz.normalize(local_dt)
+
+def aslocaltimestr(utc_dt):
+    return utc_to_local(utc_dt).strftime('%Y-%m-%d %H:%M:%S')
 
 def take():
     total = 100
     nb = 0
 
     while nb < nbPrise:
-        time.sleep(0.1)
-        total = total - GPIO.input(Trig)
+        time.sleep(0.01)
+        input = GPIO.input(Trig)
+        total = total - input
         nb = nb + 1
     return total
 
 
 while True:
     score = take()
-    print(score)
-
-
+    url = 'http://51.158.78.254:8080/measurements/'
+    payload = {'value': score, 'room' : 100, 'captorName' : 'soundOfSilence', 'date' : aslocaltimestr(datetime.datetime.now()) }
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps({'measurement' : payload}), headers=headers)
+    print(json.dumps({'measurment' :payload}))
+    print(response)
 GPIO.cleanup()
